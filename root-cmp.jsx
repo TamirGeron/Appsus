99c664b259e58aacdb5ca92f90b0b09df2214cee
TamirGeron <tamir1234432@gmail.com>
2022-04-27 10:09:21 +0300

    starting

--- root-cmp.jsx
+++ root-cmp.jsx
@@ -1,19 +1,20 @@
 import { NoteIndex } from './apps/keep/pages/note-index.jsx'
 import { MailIndex } from './apps/mail/pages/mail-index.jsx'
+import { AppHome } from './pages/app-home.jsx'
 
 const Router = ReactRouterDOM.HashRouter
 const { Route, Switch } = ReactRouterDOM
 
 export function Main() {
     return <Router>
-        <AppHeader />
+        {/* <AppHeader /> */}
         <section className="app">
             <Switch>
                 <Route path="/note" component={NoteIndex} />
                 <Route path="/mail" component={MailIndex} />
-                <Route path="/" component={Home} />
+                <Route path="/" component={AppHome} />
             </Switch>
         </section>
-        <UserMsg />
+        {/* <UserMsg /> */}
     </Router>
 }
