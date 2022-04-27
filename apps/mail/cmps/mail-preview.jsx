4abaf33156b33e85d342223682d8b6a5cb20a242
TamirGeron <tamir1234432@gmail.com>
2022-04-27 20:32:51 +0300

    search header

--- apps/mail/cmps/mail-preview.jsx
+++ apps/mail/cmps/mail-preview.jsx
@@ -5,11 +5,12 @@ export function MailPreview({ mail, onSelect }) {
 
     return <tr className={`mail-preview ${isReadClass}`}>
         <td className="check-td"><input type="checkbox" onClick={() => onSelect(mail.id)} /></td>
-        <td className="main-td"><Link to={`/mail/${mail.id}`}>
-            <div className="mail">{mail.mail}</div>
-            <div className="title">{mail.title}</div>
-            <div>{mail.body}</div>
-        </Link >
+        <td className="main-td">
+            <Link to={`/mail/${mail.id}`}>
+                <div className="mail">{mail.mail}</div>
+                <div className="title">{mail.title}</div>
+                <div>{mail.body}</div>
+            </Link >
         </td>
     </tr>
 }
--- cmps/app-header.jsx
+++ cmps/app-header.jsx
@@ -4,11 +4,11 @@ function _AppHeader(props) {
 
     return <header className="app-header">
         <h3 >Appsus</h3>
-
+        <input type="search" placeholder='Search mail' />
         <nav>
-            <NavLink to="/" exact>Home</NavLink>
-            <NavLink to="/mail">Mail</NavLink>
             <NavLink to="/note">Note</NavLink>
+            <NavLink to="/mail">Mail</NavLink>
+            <NavLink to="/" exact>Home</NavLink>
         </nav>
     </header>
 }
