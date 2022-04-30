d1328913e98d97f537dabb14eab1ca47272f832b
TamirGeron <tamir1234432@gmail.com>
2022-04-30 21:36:26 +0300

    css

--- pages/app-home.jsx
+++ pages/app-home.jsx
@@ -3,7 +3,10 @@ const { Link } = ReactRouterDOM
 export function AppHome() {
     return <section className="app-home">
         <h1>Welcome to T&S App</h1>
+        <div className="link">
         <Link to="/mail"><button>Mail App</button></Link>
         <Link to="/note"><button>Keep Notes App</button></Link>
+        </div>
+
     </section>
 }
