b975d8dd735e23c3f521be6d8f396ed2ef910724
TamirGeron <tamir1234432@gmail.com>
2022-04-30 21:57:12 +0300

    Home

--- pages/app-home.jsx
+++ pages/app-home.jsx
@@ -4,15 +4,33 @@ export function AppHome() {
     return <section className="app-home">
         {/* <h1>Welcome to T&S App</h1> */}
         <div className="link">
+            <button>
+                <Link to="/mail">
+                    Mail App
+                </Link>
+            </button>
+            <button>
+                <Link to="/note">
+                    Keep Notes App
+                </Link>
+            </button>
+            <button>
+                <a href="https://tamirgeron.github.io/book-shop/">Books </a>
+            </button>
+
+
             <Link to="/mail">
                 <img src="https://media.giphy.com/media/eRIrROHUPJvgs/giphy.gif" alt="" />
             </Link>
+
             <Link to="/note">
                 <img src="https://media.giphy.com/media/l3JE59A6vcL4cqRYbZ/giphy.gif" alt="" />
             </Link>
 
-            {/* <Link to="/mail"><button>Mail App</button></Link>
-        <Link to="/note"><button>Keep Notes App</button></Link> */}
+            <a href="https://tamirgeron.github.io/book-shop/">
+                <img src="https://media.giphy.com/media/3o85xBwvWcj1Z11Gda/giphy.gif" alt="" />
+            </a>
+
         </div>
 
     </section>
